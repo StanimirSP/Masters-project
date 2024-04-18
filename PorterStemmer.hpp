2299761526e77ed9ff6c4abb781f988c66d7b439
Lassie <lassie@m.lassie.cf>
2024-04-18 16:50:55 +0300

    improve readability of PorterStemmer.hpp

--- PorterStemmer.hpp
+++ PorterStemmer.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <cstddef>
+#include "constants.hpp"
 #include "contextualReplacementRule.hpp"
 
 namespace PorterStemmer
@@ -28,84 +29,81 @@ namespace PorterStemmer
 
 	inline const std::string m_gt_0 = "((" + C + VC + '|' + V_starting_non_y + C + ')' + VC + '*' + opt_V + ')';
 	inline const std::string m_gt_1 = "((" + C + VC + '|' + V_starting_non_y + C + ')' + VC + VC + '*' + opt_V + ')';
-	//inline const std::string m_eq_1 = '(' + whitespace + '(' + C + VC + '|' + V_starting_non_y + C + ')' + opt_V + ')';
-	inline const std::string rctx = "\x02";
+	inline const std::string lctx{Constants::Epsilon};
+	inline const std::string rctx{'\x02'};
 
 	using namespace std::string_literals;
 	inline const std::vector<ContextualReplacementRule> steps[] = {
-		/*{ // 0
-			{"[_,\x02]"s, letter + letter + letter, whitespace}, // only allow words of length > 2 to be modified
-		},*/
 		{ // 1a
-			{"[sses,ss\x02]"s, "_"s, whitespace},						// SSES -> SS
-			{"[ies,i\x02]"s, "_"s, whitespace},							// IES  -> I
-			{"[ss,ss\x02]"s, /*"_"s*/ letter, whitespace},				// SS   -> SS
-			{"[s,\x02]"s, /*"_"s*/ letter + letter, whitespace},		// S    ->
-			{"[_,\x02]"s, letter + letter + letter_not_s, whitespace},		// only allow words of length > 2 to be modified
+			{"[sses,ss\x02]"s, lctx, whitespace},						// SSES -> SS
+			{"[ies,i\x02]"s, lctx, whitespace},							// IES  -> I
+			{"[ss,ss\x02]"s, letter, whitespace},						// SS   -> SS
+			{"[s,\x02]"s, letter + letter, whitespace},					// S    ->
+			{"[_,\x02]"s, letter + letter + letter_not_s, whitespace},	// only allow words of length > 2 to be modified
 		},
 		{ // 1b -- '\x01' is used as a marker for whether step 1b' should take place
-			{"[eed,ee]"s, m_gt_0, rctx},					// (m>0) EED -> EE
-			{"[eed,eed]"s, "_"s, rctx}, 					// suppresses the rules below if the word ends in 'eed' but the condition (m>0) is not satisfied
-			{"[ed,\x01]"s, contains_vowel, rctx},			// (*v*) ED  ->
-			{"[ing,\x01]"s, contains_vowel, rctx},			// (*v*) ING ->
+			{"[eed,ee]"s, m_gt_0, rctx},				// (m>0) EED -> EE
+			{"[eed,eed]"s, lctx, rctx}, 				// suppresses the rules below if the word ends in 'eed' but the condition (m>0) is not satisfied
+			{"[ed,\x01]"s, contains_vowel, rctx},		// (*v*) ED  ->
+			{"[ing,\x01]"s, contains_vowel, rctx},		// (*v*) ING ->
 		},
 		{ // 1b'
-			{"[at\x01,ate]"s, "_"s, rctx},																// AT -> ATE
-			{"[bl\x01,ble]"s, "_"s, rctx},																// BL -> BLE
-			{"[iz\x01,ize]"s, "_"s, rctx},																// IZ -> IZE
+			{"[at\x01,ate]"s, lctx, rctx},													// AT -> ATE
+			{"[bl\x01,ble]"s, lctx, rctx},													// BL -> BLE
+			{"[iz\x01,ize]"s, lctx, rctx},													// IZ -> IZE
 			{"([bb,b]|[cc,c]|[dd,d]|[ff,f]|[gg,g]|[hh,h]|[jj,j]|[kk,k]|[mm,m]|[nn,n]"
-			 "|[pp,p]|[qq,q]|[rr,r]|[tt,t]|[vv,v]|[ww,w]|[xx,x])[\x01,_]"s, "_"s, rctx},				// (*d and not (*L or *S or *Z)) -> single letter
-			{"[\x01,e]"s, whitespace + C + vowel_or_y + consonant_not_wxy, rctx},						// (m=1 and *o) -> E
-			{"[\x01,_]"s, "_"s, rctx},																	// if none of the above can be applied, deletes marker '\x01'
+			 "|[pp,p]|[qq,q]|[rr,r]|[tt,t]|[vv,v]|[ww,w]|[xx,x])[\x01,_]"s, lctx, rctx},	// (*d and not (*L or *S or *Z)) -> single letter
+			{"[\x01,e]"s, whitespace + C + vowel_or_y + consonant_not_wxy, rctx},			// (m=1 and *o) -> E
+			{"[\x01,_]"s, lctx, rctx},														// if none of the above can be applied, deletes marker '\x01'
 		},
 		{ // 1c
-			{"[y,i]"s, /*letter + vowel_or_y + '|' + contains_vowel + letter*/ contains_vowel, rctx},	// (*v*) Y -> I
+			{"[y,i]"s, contains_vowel, rctx},	// (*v*) Y -> I
 		},
 		{ // 2
 			{"[ational,ate]"s, m_gt_0, rctx},	// (m>0) ATIONAL ->  ATE
-			{"[ational,ational]"s, "_"s, rctx},
+			{"[ational,ational]"s, lctx, rctx},
 			{"[tional,tion]"s, m_gt_0, rctx},	// (m>0) TIONAL  ->  TION
-			{"[tional,tional]"s, "_"s, rctx},
+			{"[tional,tional]"s, lctx, rctx},
 			{"[enci,ence]"s, m_gt_0, rctx},		// (m>0) ENCI    ->  ENCE
-			{"[enci,enci]"s, "_"s, rctx},
+			{"[enci,enci]"s, lctx, rctx},
 			{"[anci,ance]"s, m_gt_0, rctx},		// (m>0) ANCI    ->  ANCE
-			{"[anci,anci]"s, "_"s, rctx},
+			{"[anci,anci]"s, lctx, rctx},
 			{"[izer,ize]"s, m_gt_0, rctx},		// (m>0) IZER    ->  IZE
-			{"[izer,izer]"s, "_"s, rctx},
+			{"[izer,izer]"s, lctx, rctx},
 		//	{"[abli,able]"s, m_gt_0, rctx},		// (m>0) ABLI    ->  ABLE
-		//	{"[abli,abli]"s, "_"s, rctx},
+		//	{"[abli,abli]"s, lctx, rctx},
 			{"[bli,ble]"s, m_gt_0, rctx},		// (m>0) BLI    ->  BLE		// replaces the above rule
-			{"[bli,bli]"s, "_"s, rctx},
+			{"[bli,bli]"s, lctx, rctx},
 			{"[alli,al]"s, m_gt_0, rctx},		// (m>0) ALLI    ->  AL
-			{"[alli,alli]"s, "_"s, rctx},
+			{"[alli,alli]"s, lctx, rctx},
 			{"[entli,ent]"s, m_gt_0, rctx},		// (m>0) ENTLI   ->  ENT
-			{"[entli,entli]"s, "_"s, rctx},
+			{"[entli,entli]"s, lctx, rctx},
 			{"[eli,e]"s, m_gt_0, rctx},			// (m>0) ELI     ->  E
-			{"[eli,eli]"s, "_"s, rctx},
+			{"[eli,eli]"s, lctx, rctx},
 			{"[ousli,ous]"s, m_gt_0, rctx},		// (m>0) OUSLI   ->  OUS
-			{"[ousli,ousli]"s, "_"s, rctx},
+			{"[ousli,ousli]"s, lctx, rctx},
 			{"[ization,ize]"s, m_gt_0, rctx},	// (m>0) IZATION ->  IZE
-			{"[ization,ization]"s, "_"s, rctx},
+			{"[ization,ization]"s, lctx, rctx},
 			{"[ation,ate]"s, m_gt_0, rctx},		// (m>0) ATION   ->  ATE
-			{"[ation,ation]"s, "_"s, rctx},
+			{"[ation,ation]"s, lctx, rctx},
 			{"[ator,ate]"s, m_gt_0, rctx},		// (m>0) ATOR    ->  ATE
-			{"[ator,ator]"s, "_"s, rctx},
+			{"[ator,ator]"s, lctx, rctx},
 			{"[alism,al]"s, m_gt_0, rctx},		// (m>0) ALISM   ->  AL
-			{"[alism,alism]"s, "_"s, rctx},
+			{"[alism,alism]"s, lctx, rctx},
 			{"[iveness,ive]"s, m_gt_0, rctx},	// (m>0) IVENESS ->  IVE
-			{"[iveness,iveness]"s, "_"s, rctx},
+			{"[iveness,iveness]"s, lctx, rctx},
 			{"[fulness,ful]"s, m_gt_0, rctx},	// (m>0) FULNESS ->  FUL
-			{"[fulness,fulness]"s, "_"s, rctx},
+			{"[fulness,fulness]"s, lctx, rctx},
 			{"[ousness,ous]"s, m_gt_0, rctx},	// (m>0) OUSNESS ->  OUS
-			{"[ousness,ousness]"s, "_"s, rctx},
+			{"[ousness,ousness]"s, lctx, rctx},
 			{"[aliti,al]"s, m_gt_0, rctx},		// (m>0) ALITI   ->  AL
-			{"[aliti,aliti]"s, "_"s, rctx},
+			{"[aliti,aliti]"s, lctx, rctx},
 			{"[iviti,ive]"s, m_gt_0, rctx},		// (m>0) IVITI   ->  IVE
-			{"[iviti,iviti]"s, "_"s, rctx},
+			{"[iviti,iviti]"s, lctx, rctx},
 			{"[biliti,ble]"s, m_gt_0, rctx},	// (m>0) BILITI  ->  BLE
-			{"[biliti,biliti]"s, "_"s, rctx},
+			{"[biliti,biliti]"s, lctx, rctx},
 			{"[logi,log]"s, m_gt_0, rctx},		// (m>0) LOGI    ->  LOG	// new rule
-			{"[logi,logi]"s, "_"s, rctx},
+			{"[logi,logi]"s, lctx, rctx},
 		},
 		{ // 3
 			{"[icate,ic]"s, m_gt_0, rctx},		// (m>0) ICATE ->  IC
@@ -118,43 +116,43 @@ namespace PorterStemmer
 		},
 		{ // 4
 			{"[al,_]"s, m_gt_1, rctx},			// (m>1) AL    ->
-			{"[al,al]"s, "_"s, rctx},
+			{"[al,al]"s, lctx, rctx},
 			{"[ance,_]"s, m_gt_1, rctx},		// (m>1) ANCE  ->
-			{"[ance,ance]"s, "_"s, rctx},
+			{"[ance,ance]"s, lctx, rctx},
 			{"[ence,_]"s, m_gt_1, rctx},		// (m>1) ENCE  ->
-			{"[ence,ence]"s, "_"s, rctx},
+			{"[ence,ence]"s, lctx, rctx},
 			{"[er,_]"s, m_gt_1, rctx},			// (m>1) ER    ->
-			{"[er,er]"s, "_"s, rctx},
+			{"[er,er]"s, lctx, rctx},
 			{"[ic,_]"s, m_gt_1, rctx},			// (m>1) IC    ->
-			{"[ic,ic]"s, "_"s, rctx},
+			{"[ic,ic]"s, lctx, rctx},
 			{"[able,_]"s, m_gt_1, rctx},		// (m>1) ABLE  ->
-			{"[able,able]"s, "_"s, rctx},
+			{"[able,able]"s, lctx, rctx},
 			{"[ible,_]"s, m_gt_1, rctx},		// (m>1) IBLE  ->
-			{"[ible,ible]"s, "_"s, rctx},
+			{"[ible,ible]"s, lctx, rctx},
 			{"[ant,_]"s, m_gt_1, rctx},			// (m>1) ANT   ->
-			{"[ant,ant]"s, "_"s, rctx},
+			{"[ant,ant]"s, lctx, rctx},
 			{"[ement,_]"s, m_gt_1, rctx},		// (m>1) EMENT ->
-			{"[ement,ement]"s, "_"s, rctx},
+			{"[ement,ement]"s, lctx, rctx},
 			{"[ment,_]"s, m_gt_1, rctx},		// (m>1) MENT  ->
-			{"[ment,ment]"s, "_"s, rctx},
+			{"[ment,ment]"s, lctx, rctx},
 			{"[ent,_]"s, m_gt_1, rctx},			// (m>1) ENT   ->
-			{"[ent,ent]"s, "_"s, rctx},
+			{"[ent,ent]"s, lctx, rctx},
 			{"[ion,_]"s, "((" + C + VC + '|' + V_starting_non_y + C + ')' + VC + '*' + V + opt_C + "(s|t))", rctx},		// (m>1 and (*S or *T)) ION ->
-			{"[ion,ion]"s, "_"s, rctx},
+			{"[ion,ion]"s, lctx, rctx},
 			{"[ou,_]"s, m_gt_1, rctx},			// (m>1) OU    ->
-			{"[ou,ou]"s, "_"s, rctx},
+			{"[ou,ou]"s, lctx, rctx},
 			{"[ism,_]"s, m_gt_1, rctx},			// (m>1) ISM   ->
-			{"[ism,ism]"s, "_"s, rctx},
+			{"[ism,ism]"s, lctx, rctx},
 			{"[ate,_]"s, m_gt_1, rctx},			// (m>1) ATE   ->
-			{"[ate,ate]"s, "_"s, rctx},
+			{"[ate,ate]"s, lctx, rctx},
 			{"[iti,_]"s, m_gt_1, rctx},			// (m>1) ITI   ->
-			{"[iti,iti]"s, "_"s, rctx},
+			{"[iti,iti]"s, lctx, rctx},
 			{"[ous,_]"s, m_gt_1, rctx},			// (m>1) OUS   ->
-			{"[ous,ous]"s, "_"s, rctx},
+			{"[ous,ous]"s, lctx, rctx},
 			{"[ive,_]"s, m_gt_1, rctx},			// (m>1) IVE   ->
-			{"[ive,ive]"s, "_"s, rctx},
+			{"[ive,ive]"s, lctx, rctx},
 			{"[ize,_]"s, m_gt_1, rctx},			// (m>1) IZE   ->
-			{"[ize,ize]"s, "_"s, rctx},
+			{"[ize,ize]"s, lctx, rctx},
 		},
 		{ // 5a
 			{"[e,_]"s, m_gt_1, rctx},											// (m>1) E     ->
@@ -170,7 +168,7 @@ namespace PorterStemmer
 		{ // 5b
 			{"[l\x02,_]"s, "((" + C + VC + '|' + V_starting_non_y + C + ')' + VC + '*' + V + opt_C + "l)", whitespace},		// (m > 1 and *d and *L) -> single letter;
 																															// also deletes the marker '\x02'
-			{"[\x02,_]"s, "_"s, whitespace}, 																				// deletes the marker '\x02'
+			{"[\x02,_]"s, lctx, whitespace}, 																				// deletes the marker '\x02'
 		},
 	};
 	inline constexpr std::size_t steps_cnt = sizeof(steps) / sizeof(*steps);
